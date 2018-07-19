994c370711880f516bcddeba2a6ed4b11bd8dc7e
mihran <mihranz7@gmail.com>
2018-07-19 11:45:09 +0530

    Update ramdisk files

--- ramdisk/init.spectrum.sh
+++ ramdisk/init.spectrum.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 # SPECTRUM KERNEL MANAGER
 # Profile initialization script by nathanchance
-# Default Profile for NoName kernel is Stock Profile
 
 # If there is not a persist value, we need to set one
 if [ ! -f /data/property/persist.spectrum.profile ]; then
